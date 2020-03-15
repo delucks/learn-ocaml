e445a980aa21772a119a20417ad8ba214035dd05
delucks <ping@jamieluck.com>
2020-03-15 13:31:49 -0400

    Problems six and seven

--- lists.ml
+++ lists.ml
@@ -32,3 +32,26 @@ let reverse_list list =
     | head :: tail -> aux (head :: acc) tail in
   aux [] list;;
 ;;
+
+(*** Problem 6 ***)
+let is_palindrome list =
+  list = reverse_list list
+;;
+
+(*** Problem 7 ***)
+type 'a node =
+  | One of 'a
+  | Many of 'a node list
+;;
+
+(* this one confused me a bit; I wasn't clear on why I needed the [] case
+ * or why both One and Many require the cons syntax to destructure the input list
+ * Also, some testing confusion- I need to make sure to redeclare variables in the REPL that were declared in an earlier version of the module if I make changes when running #use "foo.ml";;
+ *)
+let flatten list =
+  let rec inner_flatten acc = function
+    | [] -> acc
+    | One x :: tail -> inner_flatten (x :: acc) tail
+    | Many x :: tail -> inner_flatten (inner_flatten acc x) tail in
+  reverse_list (inner_flatten [] list)
+;;
