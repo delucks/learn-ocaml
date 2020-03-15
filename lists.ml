5531d20c3f2014b8d1bac5e0ac4c571a01ee440f
delucks <ping@jamieluck.com>
2020-03-15 00:24:04 -0400

    Problem 5

--- lists.ml
+++ lists.ml
@@ -16,9 +16,19 @@ let rec last_two = function
 let rec at k = function
   | [] -> None
   | head :: tail -> if k = 0 then Some head else at (k-1) tail
-
+;;
 
 (*** Problem 4 ***)
 let rec length = function
   | [] -> 0
   | _ :: t -> 1 + length t
+;;
+
+(*** Problem 5 ***)
+(* This needs an auxillary function because it must track state, namely the "acc"umulator list which keeps all reversed elements. This builds a call stack until the "tail" var is [], popping back up the call stack and appending an element to the acc list each time *)
+let reverse_list list =
+  let rec aux acc = function
+    | [] -> acc
+    | head :: tail -> aux (head :: acc) tail in
+  aux [] list;;
+;;
